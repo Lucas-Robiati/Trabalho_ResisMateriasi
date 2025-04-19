bc43fbea2c49a0f5b906f6785a72255ea61b399a
Lucas-Robiati <lucas.santana.280304@gmail.com>
2025-04-19 13:05:41 -0300

    inicio da interface, sem tempo pra mexer mais

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import tkinter as tk
+#fazer botão tela cheia
+#self.root.attributes('-fullscreen', True)
 
-janela = tk.Tk()
-janela.title("Minha Janela Tkinter")
-janela.geometry("300x100+20+20")
 
-msg = tk.Label(janela, text="test")
-msg.pack()
-janela.mainloop()
+from tkinter import *
+
+root = Tk()
+
+class Application():
+    def __init__(self):
+        self.root = root
+        self.window()
+        root.mainloop()
+
+    def window(self):
+        self.root.title("Calculadora de Momento de Inercia")
+        self.root.configure(background='#5e5c64')
+        self.root.geometry("700x500")
+        self.root.resizable(True,True)
+
+Application()
